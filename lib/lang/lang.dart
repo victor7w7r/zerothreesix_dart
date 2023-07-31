b323e2aa4c539400f0725e274e1eb2da64a4cc62
victor7w7r <43785784+victor7w7r@users.noreply.github.com>
2023-07-31 14:26:40 -0500

    UpdateLang

--- lib/lang/lang.dart
+++ lib/lang/lang.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: avoid_function_literals_in_foreach_calls
 import 'dart:io' show exit;
 
 import 'package:console/console.dart' show Console;
@@ -15,30 +16,60 @@ bool english = false;
 final _dictEsp = <String>[];
 final _dictEng = <String>[];
 
+final _dictDialogEsp = <String>[];
+final _dictDialogEng = <String>[];
+
+String _replaceCustom(
+  final String sel,
+  final String? custom
+) => custom != null
+  ? sel.replaceAll('*', custom)
+  : sel;
+
+void setDictDialogEsp(final List<String> incEsp) =>
+  _dictDialogEsp.addAll(incEsp);
+
+void setDictDialogEng(final List<String> incEng) =>
+  _dictDialogEng.addAll(incEng);
+
 void setDictEsp(final List<String> incEsp) =>
   _dictEsp.addAll(incEsp);
 
 void setDictEng(final List<String> incEng) =>
   _dictEng.addAll(incEng);
 
-String lang(
+String dialogLang(
   final int index,
-  [final PrintQuery? typeQuery]
-) => IO(() =>
-  english ? _dictEng[index] : _dictEsp[index]
+  [final String? ins]
+) => IO(() => english
+  ? _dictDialogEng[index]
+  : _dictDialogEsp[index]
+).map((final sel) => ins != null
+  ? sel.replaceAll('*', ins)
+  : sel
+).run();
+
+String lang(
+  final int index, [
+    final PrintQuery? typeQuery,
+    final String? custom
+  ]
+) => IO(() => english
+  ? _dictEng[index]
+  : _dictEsp[index]
 ).map((final sel) => typeQuery != null
   ? IO(() => switch(typeQuery) {
       PrintQuery.normal =>
-        print(sel),
+        print(_replaceCustom(sel, custom)),
       PrintQuery.inline =>
-        Console.write(sel),
+        Console.write(_replaceCustom(sel, custom)),
       PrintQuery.warn =>
-        print('${cyan('[*] ')} WARNING: $sel'),
+        print('${cyan('[*] ')} WARNING: ${_replaceCustom(sel, custom)}'),
       PrintQuery.error =>
-        print('${red('[*] ')} ERROR: $sel')
+        print('${red('[*] ')} ERROR: ${_replaceCustom(sel, custom)}')
     })
     .map((final _) => '').run()
-  : sel
+  : _replaceCustom(sel, custom)
 ).run();
 
 void error(final int index) {
@@ -51,5 +82,11 @@ void error(final int index) {
 void ok(final int index) {
   clear();
   lang(index, PrintQuery.normal);
+  exit(0);
+}
+
+void okLine(){
+  print('\n =============== OK =============== \n');
+  lang(5, PrintQuery.normal);
   exit(0);
 }
